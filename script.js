8cf0ab63e7c1827329fa98c3697405759ea3441f
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2022-06-02 12:56:38 -0400

    Creating http request

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 
 // I declare my url
 const url = "https://blockchain.info/ticker";
+
+// I create a request with JS, 
+let request = new XMLHttpRequest();    // Create an object
+request.open("GET", url);             // open a new request that will use url to access API
+request.responseType = "json";       // I want the date to be in Json format
+request.send();
