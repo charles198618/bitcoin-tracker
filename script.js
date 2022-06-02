0fbfaf174e159ea03967ffb122251974a286d374
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2022-06-02 13:10:47 -0400

    Creating a function to post the APi data

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ const url = "https://blockchain.info/ticker";
 let request = new XMLHttpRequest();    // Create an object
 request.open("GET", url);             // open a new request that will use url to access API
 request.responseType = "json";       // I want the date to be in Json format
-request.send();
+request.send();                     // I send my request
+
+//Once I receive a response, this function must be executed.
+request.onload = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            let storeResponse = request.response;  // Store the response
+            console.log(storeResponse);
+        }
+    }
+}
